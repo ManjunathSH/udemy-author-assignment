2d98eaa36a56932af028b6ae4b70de46098b4965
ManjunathSH <manjunaths.horapati@gmail.com>
2020-04-12 18:49:44 +0530

    authors-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AuthorsComponent } from './components/authors/authors.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorsComponent
   ],
   imports: [
     BrowserModule
